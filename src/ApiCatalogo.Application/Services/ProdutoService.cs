c35df7c55997f5e701dd0e1cfa821681ce37c26e
André Luiz Braga <andreluizcb2@gmail.com>
2025-04-23 20:57:37 -0300

    novas alteracoes controller produto

---
+++ src/ApiCatalogo.Application/Services/ProdutoService.cs
@@ -0,0 +1,61 @@
+﻿using ApiCatalogo.Application.Services.Interfaces;
+using ApiCatalogo.Domain.Entities;
+using ApiCatalogo.Domain.Interfaces;
+
+namespace ApiCatalogo.Application.Services
+{
+    public class ProdutoService : IProdutoService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        public ProdutoService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+        public IEnumerable<Produto> GetProdutos()
+        {
+            var produtos = _produtoRepository.GetProdutos().ToList();
+            return produtos;
+        }
+        public async Task<Produto> GetProdutoById(int id)
+        {
+            var produto = await _produtoRepository.GetProduto(id);
+            return produto;
+        }
+
+        public Produto Create(Produto produto)
+        {
+            var produto_ = _produtoRepository.Create(produto);
+            return produto_;
+
+        }
+        public bool Update(Produto produto)
+        {
+            var produto_ = _produtoRepository.Update(produto);
+
+            if (produto_ == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            var produto_ = _produtoRepository.Delete(id);
+
+            if (produto_ == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+
+
+
+    }
+}
--- src/ApiCatalogo.Domain/Interfaces/IProdutoRepository.cs
+++ src/ApiCatalogo.Domain/Interfaces/IProdutoRepository.cs
@@ -1,13 +1,16 @@
-﻿
-
-
-using ApiCatalogo.Domain.Entities;
+﻿using ApiCatalogo.Domain.Entities;
 
 namespace ApiCatalogo.Domain.Interfaces
 {
     public interface IProdutoRepository
     {
-        Task<Produto?> ObterProdutoPorIdAsync(int produtoId);
-        Task<IEnumerable<Produto?>> ObterProdutosAsync();
+        // para exemplo vai ser usado o Iqueriable e retornos bool como opção a mais de uso para cada contexto.
+
+        IQueryable<Produto> GetProdutos();
+        Task<Produto> GetProduto(int id);
+        Produto Create(Produto produto);
+        bool Update(Produto produto);
+        bool Delete(int id);
+
     }
 }
